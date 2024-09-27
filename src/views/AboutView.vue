d462dbf589ea0c2bd0f293ed608b169b00ccf9fa
Prof. Hugo <hugo.tannus@go.senac.br>
2024-09-27 09:49:49 -0300

    Primeiro commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <h1>Sobre</h1>
+    <p>Trabalho da turma Técnico em Informática - 2023.</p>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>Página Principal</h1>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView'
+}
+</script>
+
+
+<style>
+@media (min-width: 1024px) {
+  main {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
