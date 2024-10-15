ce82ba186ba85d3eb880edbc3bbe6850cfa0c06f
Hugo Leonardo <hugao.leo123@gmailcom>
2024-10-15 11:15:14 -0300

    Componene NavBar Criado.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,29 @@
 <template>
-  <NCContainer>
-    <header>
-      <NCContainer>
-        <div class="wrapper">
-          <MainPage title="Nutri Calc" />
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">Sobre</RouterLink>
-            <RouterLink to="/calculation">Cálculos</RouterLink>
-            <RouterLink to="/diet">Dietas</RouterLink>
-            <RouterLink to="/registration">Registro do Usuário</RouterLink>
-      </nav>
-        </div>
-      </NCContainer>
-    </header>
-    <main>
-      <NCContainer>
-        <RouterView />
-      </NCContainer>
-    </main>
-  </NCContainer>
+  <header>
+    <TheNavBar>
+      <div class="wrapper">
+        <MainPage title="Nutri Calc" />
+        <nav>
+          <RouterLink to="/">Home</RouterLink>
+          <RouterLink to="/about">Sobre</RouterLink>
+          <RouterLink to="/calculation">Cálculos</RouterLink>
+          <RouterLink to="/diet">Dietas</RouterLink>
+        </nav>
+      </div>
+    </TheNavBar>
+  </header>
+  <main>
+    <NCContainer>
+      <RouterView />
+    </NCContainer>
+  </main>
 </template>
 
 <script>
 import { RouterLink, RouterView } from 'vue-router'
 import NCContainer from './components/NCContainer.vue'
 import TheMainPage from './components/TheMainPage.vue'
+import TheNavBar from './components/TheNavBar.vue';
 
 export default {
   name: 'App',
@@ -34,6 +32,7 @@ export default {
     RouterLink,
     RouterView,
     NCContainer,
+    TheNavBar,
   }
 }
 </script>
@@ -43,44 +42,48 @@ header {
   line-height: 1.5;
   max-height: 100vh;
 }
+
 .logo {
   display: block;
   margin: 0 auto 2rem;
 }
+
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
 }
+
 nav a.router-link-exact-active {
   color: var(--color-text);
 }
+
 nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
+
 nav a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
+
 nav a:first-of-type {
   border: 0;
 }
+
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
+
   .logo {
     margin: 0 2rem 0 0;
   }
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+
   nav {
     text-align: left;
     margin-left: -1rem;
---
+++ src/components/TheNavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+        <NCContainer>
+            <div>
+                <slot></slot>
+            </div>
+        </NCContainer>
+    </nav>
+</template>
+
+<script>
+import NCContainer from './NCContainer.vue';
+
+export default {
+    components: {
+        NCContainer,
+    },
+}
+</script>
