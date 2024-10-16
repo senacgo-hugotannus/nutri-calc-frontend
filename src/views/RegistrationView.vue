3ebd10f4cdb12d5973867275e2b50304135bbeaf
carlos-henrique08 <carloshenriqueds2004@gmail.com>
2024-10-16 10:58:41 -0300

    criaçao da pagina de registro dos Usuários

---
+++ src/views/RegistrationView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Registro do Usuário</h1>
+      <p></p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'RegistrationView'
+  }
+  </script>
