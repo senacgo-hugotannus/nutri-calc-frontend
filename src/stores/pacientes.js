c61b08124a2e2159d0061aeaffc5f16646b7d600
Vicky <nelsaalves14@gmail.com>
2024-10-18 11:54:14 -0300

    aplica estado de registro na pagina RegistrationView

--- src/stores/pacientes.js
+++ src/stores/pacientes.js
@@ -9,12 +9,12 @@ export const pacienteStore = defineStore('paciente', {
       peso: 70,
       fatorAtividade: 1.6,
       registros: [
-        { peso: 90, fatorAtividade: 'Atividade Moderada', data: '10/10/2024' },
-        { peso: 92, fatorAtividade: 'Atividade Intensa', data: '28/09/2024' },
-        { peso: 97, fatorAtividade: 'Atividade Leve', data: '13/09/2024' },
-        { peso: 100, fatorAtividade: 'Atividade Leve', data: '20/08/2024' },
-        { peso: 98, fatorAtividade: 'Atividade Moderada', data: '10/08/2024' },
-        { peso: 96, fatorAtividade: 'Atividade Leve', data: '29/07/2024' },
+        { peso: 90, fatorAtividade: 'Atividade Moderada', date: '10/10/2024' },
+        { peso: 92, fatorAtividade: 'Atividade Intensa', date: '28/09/2024' },
+        { peso: 97, fatorAtividade: 'Atividade Leve', date: '13/09/2024' },
+        { peso: 100, fatorAtividade: 'Atividade Leve', date: '20/08/2024' },
+        { peso: 98, fatorAtividade: 'Atividade Moderada', date: '10/08/2024' },
+        { peso: 96, fatorAtividade: 'Atividade Leve', date: '29/07/2024' },
       ],
     }
   },
