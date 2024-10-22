26b966465ddbf1acf5221927bd03186bf95d4803
zGuxta <gustavo15zucchini@gmail.com>
2024-10-22 09:19:56 -0300

    navbar no topo mas não alinhada

--- src/App.vue
+++ src/App.vue
@@ -73,9 +73,12 @@ nav a:first-of-type {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
+    position: fixed;
+    top: 0;
+    text-align: center;
   }
 
-   .logo {
+  .logo {
     margin: 0 2rem 0 0;
   }
 
